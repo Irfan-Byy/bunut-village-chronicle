25a021a09c1d17ce83ebc84ede29afd121faed67
Irfan <irfansuryani67@gmail.com>
2025-09-04 08:08:43 +0700

    build: deploy to gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-import { Navigation } from "@/components/Navigation";
-import { Footer } from "@/components/Footer";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 // Layouts
@@ -36,7 +34,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* 🔥 Ganti BrowserRouter jadi HashRouter */}
+      <HashRouter>
         <Routes>
           {/* 🌐 Public routes */}
           <Route element={<PublicLayout />}>
@@ -68,7 +67,7 @@ const App = () => (
           {/* Catch All */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
